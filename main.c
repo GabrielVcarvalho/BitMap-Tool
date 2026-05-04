e6bf7c36b3d2cc31c03b5325c1b4c212a5ce3f8c
Gabriel Vitor <gabrielvcarvalho09@gmail.com>
2026-05-03 21:01:33 -0300

    feat: implement bmp FILEHEADER parsing

--- main.c
+++ main.c
@@ -2,21 +2,35 @@
 #include <stdlib.h>
 
 typedef unsigned char BYTE;
-#define HEADERSIZE 54
+#define FILEHEADERSIZE 14
 
 int isBmp (BYTE firstByte, BYTE secondByte);
+int analyseFileHeader(FILE* bmpFile, int* bmpFileSize, int* bmpOffSetPosition);
+int constructInt(BYTE* startByte);
 
 int main(int argc, char *argv[])
 {
     FILE *bmpFile = fopen("imagem.bmp", "rb");
-    BYTE header[HEADERSIZE];
+    int bmpFileSize;
+    int bmpOffSetPosition;
+
+    if(analyseFileHeader(bmpFile, &bmpFileSize, &bmpOffSetPosition) == 1)
+        return 1;
+
+    fclose(bmpFile);
+    return 0;
+}
+
+int analyseFileHeader(FILE* bmpFile, int* bmpFileSize, int* bmpOffSetPosition)
+{
+    BYTE header[FILEHEADERSIZE];
 
     if(bmpFile == NULL)
         return 1;
 
-    if(fread(header, HEADERSIZE, 1, bmpFile) != 1)
+    if(fread(header, FILEHEADERSIZE, 1, bmpFile) != 1)
     {
-        printf("Erro ao ler o header\n");
+        printf("The program could not read header\n");
         return 1;
     }
 
@@ -26,8 +40,22 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    fclose(bmpFile);
-    return 0;
+    *bmpFileSize = constructInt(&header[2]);
+    *bmpOffSetPosition = constructInt(&header[10]);
+}
+
+int constructInt(BYTE* startByte)
+{
+    int byteSize = 8;
+    int bytesInOneInt = 4;
+    int finalInt = 0;
+
+    for(int i = 0; i < bytesInOneInt; i++)
+    {
+        finalInt |= (*(startByte + i) << (byteSize * i));
+    }
+    
+    return finalInt;
 }
 
 int isBmp (BYTE firstByte, BYTE secondByte)
