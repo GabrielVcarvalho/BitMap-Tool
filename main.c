b405ab42c79e83238e71b87fc8b437fa3a57fb96
Gabriel Vitor <gabrielvcarvalho09@gmail.com>
2026-05-19 22:52:44 -0300

    refactor: rearrange methods to make main shorter and improve legibility

--- main.c
+++ main.c
@@ -13,11 +13,18 @@ typedef struct
 #define FILEHEADERSIZE 14
 #define INFOHEADERSIZE 40
 
+//File Analyse
 int isBmp (BYTE firstByte, BYTE secondByte);
 int analyseFileHeader(FILE* bmpFile, int* bmpFileSize, int* bmpOffSetPosition);
 int analyseInfoHeader(FILE* bmpFile, int* bmpImageHeight, int* bmpImageWidth, short* bitsByPixel, int* compression, int* bmpImageSize);
-int transformGrayScale(FILE* bmpFile ,int bmpOffSetPosition, int bitsPerPixel, int height, int width);
-int horizontalFlip(FILE* bmpFile, int bmpOffSetPosition, int bitsPerPixel, int height, int width);
+int isSupportedBmpFile(FILE* bmpFile, int* bmpFileSize, int* bmpOffSetPosition,
+    int* imageBmpHeight, int* imageBmpWidth, short* bitsPerPixel, int* compression, int* bmpSizeImage);
+
+//Filters
+int transformGrayScale(FILE* bmpFile ,int bmpOffSetPosition, int bitsPerPixel, int height, int width, int bytesPerRow, int padding);
+int horizontalFlip(FILE* bmpFile, int bmpOffSetPosition, int bitsPerPixel, int height, int width, int bytesPerRow, int padding);
+
+//Helpers
 int copyHeader(FILE* referenceBmp, FILE* outPutBmp, int bmpOffSetPosition);
 short constructShort(BYTE* startByte);
 int constructInt(BYTE* startByte);
@@ -32,6 +39,8 @@ int main(int argc, char *argv[])
     short bitsPerPixel;
     int compression;
     int bmpSizeImage;
+    int bytesPerRow;
+    int padding;
 
     if (argc < 3)
     {
@@ -41,32 +50,14 @@ int main(int argc, char *argv[])
 
     bmpFile = fopen(argv[1], "rb");
 
-    if(analyseFileHeader(bmpFile, &bmpFileSize, &bmpOffSetPosition) != 0)
+    if(bmpFile == NULL)
         return 1;
 
-    if(analyseInfoHeader(bmpFile, &imageBmpHeight, &imageBmpWidth, &bitsPerPixel, &compression, &bmpSizeImage) != 0)
+    if(isSupportedBmpFile(bmpFile, &bmpFileSize, &bmpOffSetPosition, &imageBmpHeight, &imageBmpWidth, &bitsPerPixel, &compression, &bmpSizeImage) != 0)
         return 1;
 
-    if(compression != 0)
-    {
-        printf("Have not suport to compressed file\n");
-        fclose(bmpFile);
-        return 1;
-    }
-
-    if(bitsPerPixel != 24)
-    {
-        printf("Images with less or more than three bytes per pixel are not supported\n");
-        fclose(bmpFile);
-        return 1;
-    }
-
-    if(bmpOffSetPosition < 54)
-    {
-        printf("Images with compressed header are not supported\n");
-        fclose(bmpFile);
-        return 1;
-    }
+    bytesPerRow = imageBmpWidth * (bitsPerPixel / 8); 
+    padding = (4 - (bytesPerRow % 4)) % 4;
 
     if(strcmp(argv[2], "grayScale") == 0)
     {
@@ -75,7 +66,9 @@ int main(int argc, char *argv[])
             bmpOffSetPosition,
             bitsPerPixel,
             imageBmpHeight,
-            imageBmpWidth
+            imageBmpWidth,
+            bytesPerRow,
+            padding
         );
             
             printf("Sucess to read and copy image\n");
@@ -87,7 +80,9 @@ int main(int argc, char *argv[])
         bmpOffSetPosition,
         bitsPerPixel,
         imageBmpHeight,
-        imageBmpWidth
+        imageBmpWidth,
+        bytesPerRow,
+        padding
     );
 
     printf("Sucess to horizontal flip image\n");
@@ -101,113 +96,9 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-int transformGrayScale(FILE* bmpFile ,int bmpOffSetPosition, int bitsPerPixel, int height, int width)
-{
-    FILE* bmpGrayScale = fopen("imagemGray.bmp", "wb");
-    int bytesPerRow;
-    int padding;
-
-    bytesPerRow = width * (bitsPerPixel / 8); 
-    padding = (4 - (bytesPerRow % 4)) % 4;
-
-    if(copyHeader(bmpFile, bmpGrayScale, bmpOffSetPosition) != 0)
-        return 1;
-    
-    for (int line = 0; line < height; line++)
-    {
-        PIXEL imageLineArray[width];
-
-        fread(imageLineArray, bytesPerRow, 1, bmpFile);
-
-        for(int i = 0; i < width; i++)
-        {
-            PIXEL pixel = imageLineArray[i];
-            int gray = (pixel.red + pixel.green + pixel.blue) / 3;
-            
-            imageLineArray[i].blue = gray;
-            imageLineArray[i].green = gray;
-            imageLineArray[i].red = gray;
-        }
-
-        fwrite(imageLineArray, bytesPerRow, 1, bmpGrayScale);
-
-        for(int i = 0; i < padding; i++)
-            fputc(0, bmpGrayScale);
-
-        fseek(bmpFile, padding, SEEK_CUR);
-    }
-    fclose(bmpGrayScale);
-    return 0;
-}
-
-int horizontalFlip(FILE* bmpFile, int bmpOffSetPosition, int bitsPerPixel, int height, int width)
-{
-    FILE* bmpHorizontalFlip = fopen("HorizontalFlipImage.bmp", "wb");
-
-    int bytesPerRow;
-    int padding;
-
-    bytesPerRow = width * (bitsPerPixel / 8);
-
-    padding = (4 - (bytesPerRow % 4)) % 4;
-
-    if(copyHeader(bmpFile, bmpHorizontalFlip, bmpOffSetPosition) != 0)
-        return 1;
-
-    PIXEL bmpRowArray[width];
-
-    int leftPixelPos;
-    int rightPixelPos;
-
-    for(int y = 0; y < height; y++)
-    {
-        fread(bmpRowArray, bytesPerRow, 1, bmpFile);
-
-        for(int x = 0; x < width / 2; x++)
-        {
-            leftPixelPos = x;
-            rightPixelPos = width - 1 - x;
-
-            PIXEL tempPixel = bmpRowArray[leftPixelPos];
-
-            bmpRowArray[leftPixelPos] = bmpRowArray[rightPixelPos];
-
-            bmpRowArray[rightPixelPos] = tempPixel;
-        }
-
-        fwrite(bmpRowArray, bytesPerRow, 1, bmpHorizontalFlip);
-
-        for(int i = 0; i < padding; i++)
-            fputc(0, bmpHorizontalFlip);
-
-        fseek(bmpFile, padding, SEEK_CUR);
-    }
-
-    fclose(bmpHorizontalFlip);
-
-    return 0;
-}
-
-int copyHeader(FILE* referenceBmp, FILE* outPutBmp, int bmpOffSetPosition)
-{
-    BYTE allHeader[bmpOffSetPosition];
-    fseek(referenceBmp, 0, SEEK_SET);
-    fread(allHeader, bmpOffSetPosition, 1, referenceBmp);
-
-    if(outPutBmp == NULL)
-        return 1;
-
-    fwrite(allHeader, bmpOffSetPosition, 1, outPutBmp);
-
-    return 0;
-}
-
 int analyseFileHeader(FILE* bmpFile, int* bmpFileSize, int* bmpOffSetPosition)
 {
     BYTE fileHeader[FILEHEADERSIZE];
-
-    if(bmpFile == NULL)
-        return 1;
 
     if(fread(fileHeader, FILEHEADERSIZE, 1, bmpFile) != 1)
     {
@@ -251,6 +142,118 @@ int analyseInfoHeader(FILE* bmpFile, int* bmpImageHeight, int* bmpImageWidth, sh
     return 0;
 }
 
+int isSupportedBmpFile(
+    FILE* bmpFile,
+    int* bmpFileSize,
+    int* bmpOffSetPosition,
+    int* imageBmpHeight,
+    int* imageBmpWidth,
+    short* bitsPerPixel,
+    int* compression,
+    int* bmpSizeImage)
+{
+    if(analyseFileHeader(bmpFile, bmpFileSize, bmpOffSetPosition) != 0)
+        return 1;
+
+    if(analyseInfoHeader(bmpFile, imageBmpHeight, imageBmpWidth, bitsPerPixel, compression, bmpSizeImage) != 0)
+        return 1;
+
+    if(*compression != 0)
+    {
+        printf("Have not suport to compressed file\n");
+        return 1;
+    }
+
+    if(*bitsPerPixel != 24)
+    {
+        printf("Images with less or more than three bytes per pixel are not supported\n");
+        return 1;
+    }
+
+    if(*bmpOffSetPosition < 54)
+    {
+        printf("Image with not supported header type\n");
+        return 1;
+    }
+
+    return 0;
+}
+
+int transformGrayScale(FILE* bmpFile ,int bmpOffSetPosition, int bitsPerPixel, int height, int width, int bytesPerRow, int padding)
+{
+    FILE* bmpGrayScale = fopen("imagemGray.bmp", "wb");
+
+    if(copyHeader(bmpFile, bmpGrayScale, bmpOffSetPosition) != 0)
+        return 1;
+    
+    for (int line = 0; line < height; line++)
+    {
+        PIXEL imageLineArray[width];
+
+        fread(imageLineArray, bytesPerRow, 1, bmpFile);
+
+        for(int i = 0; i < width; i++)
+        {
+            PIXEL pixel = imageLineArray[i];
+            int gray = (pixel.red + pixel.green + pixel.blue) / 3;
+            
+            imageLineArray[i].blue = gray;
+            imageLineArray[i].green = gray;
+            imageLineArray[i].red = gray;
+        }
+
+        fwrite(imageLineArray, bytesPerRow, 1, bmpGrayScale);
+
+        for(int i = 0; i < padding; i++)
+            fputc(0, bmpGrayScale);
+
+        fseek(bmpFile, padding, SEEK_CUR);
+    }
+    fclose(bmpGrayScale);
+    return 0;
+}
+
+int horizontalFlip(FILE* bmpFile, int bmpOffSetPosition, int bitsPerPixel, int height, int width, int bytesPerRow, int padding)
+{
+    FILE* bmpHorizontalFlip = fopen("HorizontalFlipImage.bmp", "wb");
+
+    if(copyHeader(bmpFile, bmpHorizontalFlip, bmpOffSetPosition) != 0)
+        return 1;
+
+    PIXEL bmpRowArray[width];
+
+    int leftPixelPos;
+    int rightPixelPos;
+
+    for(int y = 0; y < height; y++)
+    {
+        fread(bmpRowArray, bytesPerRow, 1, bmpFile);
+
+        for(int x = 0; x < width / 2; x++)
+        {
+            leftPixelPos = x;
+            rightPixelPos = width - 1 - x;
+
+            PIXEL tempPixel = bmpRowArray[leftPixelPos];
+
+            bmpRowArray[leftPixelPos] = bmpRowArray[rightPixelPos];
+
+            bmpRowArray[rightPixelPos] = tempPixel;
+        }
+
+        fwrite(bmpRowArray, bytesPerRow, 1, bmpHorizontalFlip);
+
+        for(int i = 0; i < padding; i++)
+            fputc(0, bmpHorizontalFlip);
+
+        fseek(bmpFile, padding, SEEK_CUR);
+    }
+
+    fclose(bmpHorizontalFlip);
+
+    return 0;
+}
+
 int constructInt(BYTE* startByte)
 {
     int byteSize = 8;
@@ -263,6 +266,20 @@ int constructInt(BYTE* startByte)
     }
     
     return finalInt;
+}
+
+int copyHeader(FILE* referenceBmp, FILE* outPutBmp, int bmpOffSetPosition)
+{
+    BYTE allHeader[bmpOffSetPosition];
+    fseek(referenceBmp, 0, SEEK_SET);
+    fread(allHeader, bmpOffSetPosition, 1, referenceBmp);
+
+    if(outPutBmp == NULL)
+        return 1;
+
+    fwrite(allHeader, bmpOffSetPosition, 1, outPutBmp);
+
+    return 0;
 }
 
 short constructShort(BYTE* startByte)
